19053bda867f06877832c6ea7fe5bdc56ad7c087
kaztro <00229017@uca.edu.sv>
2018-10-26 01:39:52 -0600

    Finished

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ Expresion Regular utilizada:
 1- /d(b+)d/g
 Cadena a probar: 
 1- cdbbdbsbz
-2- dbbd
+2- dbbbd
 ****El uso de la función constructora (new RegExp()) ofrece la compilación en tiempo de ejecución de la expresión regular. 
 Utilice la función constructora cuando sepa que el patrón de la expresión regular cambiará, o cuando desconozca el patrón 
 y deba obtenerlo de otra fuente, como por ejemplo del usuario.****/
-var myRe = new RegExp('/d(b+)d/', 'g');
+var myRe = new RegExp('/d(b+)d/', 'g');  //-> debe llevar 'd' al inicio, grupo de captura en el que pueden ir 1 o mas 'b' y debe llevar 'd' al final
 var myArray = myRe.exec('cdbbdbsbz');
-var myArray2 = /d(b+)d/g.exec('dbbd'); 
+var myArray2 = /d(b+)d/g.exec('dbbbd'); 
 console.log(myArray); //null, puede interpretarse como false es decir, que la cadena no coincidio
-console.log(myArray2); //[ "dbbd", "bb" ]
+console.log(myArray2); //[ "dbbdb", "bbb" ]
 
 /*
 Ejemplo #2 con el metodo test()
@@ -25,12 +25,12 @@ Expresion Regular utilizada:
 1-/\w+\s/g
 Cadena a probar:
 1- wwwcanales
-2- laweafome
+2- la wea fome
 */
-var myRe2 = new RegExp('/\w+\s/', 'g');
+var myRe2 = new RegExp('/\w+\s/', 'g'); //-> uno o mas caracteres de palabras que tengan un espacio en blanco despues 
 var tested = myRe2.test('wwwcanales');
-var tested1 = myRe2.test('laweafome');
-console.log(tested); //true
+var tested1 = myRe2.test('la wea fome');
+console.log(tested); //false
 console.log(tested1); //false
 
 /* 
@@ -40,4 +40,22 @@ Cuando necesite trabajar con los grupos e indices coincidentes entonces test es 
 Entre estos dos metodos no importa mucho la velocidad o la eficiencia sino que se evalue la expresion.  
 */
 
-var regexpr = new RegExp('', 'g');
+/*
+Ejemplo #3 utilizando ambos
+Expresiones utilizadas: 
+1-/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/
+2-/[a-z|A-Z]+/
+*/
+var user = prompt('Ingresa un nombre de usuario', 'Lolito');
+var pass = prompt('Ingresa una contraseña');
+
+var valUser = new RegExp('/[a-z|A-Z]+/', 'g'); //->Uno o varios caracteres de la 'a' a la 'z' mayusculas o minisculas 
+var valPass = new RegExp('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/', 'g');
+//->Minimo 8 caracteres, maximo 15, al menos una letra mayúscula o minuscula, al menos un digito, sin espacios en blanco y un caracter especial 
+(valUser.exec(user) != null)
+? console.log('Tu nombre cumple con el requisito')
+: console.log('Tu nombre no cumple con el requisito...');
+
+(valPass == true)
+? console.log('Tu contrasenea es muy fuerte!')
+: console.log('Alguien quiere ser suplantado...');
